b71590bcf827faf71d257e543626b6bb337fee0d
AFNANAMIN <33934146+AFNANAMIN@users.noreply.github.com>
2021-05-31 10:48:50 +0500

    Update First_task.cpp

--- First_task.cpp
+++ First_task.cpp
@@ -19,50 +19,50 @@ void main(){
         thread(print, i, images[i]).join();
     }
 
-    // for(thread &thread: threads){
+     // for(thread &thread: threads){
     //     thread.join();
     // }
 
 }
 
 
-// void downloadAndDistributeImage(){
+ void downloadAndDistributeImage(){
 // 	// create states, and iterate the loop.
-//     byte[] imageBytes = downloadImage();
-//     sendToChange(imageBytes);
-// }
+     byte[] imageBytes = downloadImage();
+     sendToChange(imageBytes);
+ }
 
-// byte[] downloadImage() {
+ byte[] downloadImage() {
 // 	// download the random image and get the image data as bytes
 
-// 	try
-// 	{
-// 		curlpp::Cleanup myCleanup;
-// 		int i = rand() % 100;
-// 		string url = format("https://picsum.photos/id/{}/200/", i);
-// 		curlpp::options::Url myUrl(url);
-// 		curlpp::Easy myRequest;
-// 		myRequest.setOpt(myUrl);
-// 		myRequest.perform();
+ 	try
+ 	{
+		curlpp::Cleanup myCleanup;
+ 		int i = rand() % 100;
+ 		string url = format("https://picsum.photos/id/{}/200/", i);
+ 		curlpp::options::Url myUrl(url);
+ 		curlpp::Easy myRequest;
+ 		myRequest.setOpt(myUrl);
+ 		myRequest.perform();
 
-// 		ostringstream os;
-// 		curlpp::options::WriteStream ws(&os);
-// 		myRequest.setOpt(ws);
-// 		myRequest.perform();
-// 		os << myRequest;
+ 		ostringstream os;
+ 		curlpp::options::WriteStream ws(&os);
+ 		myRequest.setOpt(ws);
+ 		myRequest.perform();
+ 		os << myRequest;
 
-// 	catch(curlpp::RuntimeError & e)
-// 	{
-// 		cout << e.what() << endl;
-// 	}
+ 	catch(curlpp::RuntimeError & e)
+ 	{
+ 		cout << e.what() << endl;
+ 	}
 
-// 	catch(curlpp::LogicError & e)
-// 	{
-// 		cout << e.what() << endl;
-// 	}
-// }
+ 	catch(curlpp::LogicError & e)
+ 	{
+ 		cout << e.what() << endl;
+ 	}
+ }
 
-// void sendToChange(byte[] imageBytes){
-// 	// create and open scocket channel on prot 4200, and then write the bytes 
-// }
+ void sendToChange(byte[] imageBytes){
+ 	// create and open scocket channel on prot 4200, and then write the bytes 
+ }
 
